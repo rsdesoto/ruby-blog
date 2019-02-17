d98fa9389998c6b981679c74db9a060e2760befe
Ry <ry.e.desoto@gmail.com>
2019-02-17 16:11:36 -0600

    pages routes set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
+
+  get "/pages", to: "pages#index"
+  # the get request for pages should go to the pages index method
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_182857) do
+ActiveRecord::Schema.define(version: 2019_02_17_220442) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
