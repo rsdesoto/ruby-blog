5016a1147efd9b078c60972e8c5e2942ec26ea12
Ry <ry.e.desoto@gmail.com>
2019-02-17 20:38:05 -0600

    added new page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,11 @@ class PagesController < ApplicationController
     end
 
     def create
-        @page = Page.new(params)
-        render plain: params.to_json
+        # @page = Page.new(params)
+        # render plain: params.class
+        page_params = params.require(:page).permit(:title, :body, :slug)
+        @page = Page.new(page_params)
+        @page.save
+        redirect_to @page
     end
 end
