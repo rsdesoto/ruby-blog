6f9d7ed571169c917c64b9117cfa3f5509658e6b
Ry <ry.e.desoto@gmail.com>
2019-02-17 19:03:54 -0600

    added moer routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,19 @@ class PagesController < ApplicationController
     def index
         @pages = Page.all
     end
+
+    def show
+        # render plain: params[:id]
+        @page = Page.find(params[:id])
+        # render plain: @page.title
+    end
+
+    def new
+        @page = Page.new
+    end
+
+    def create
+        @page = Page.new(params)
+        render plain: params.to_json
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :posts
 
   get "/pages", to: "pages#index"
+  post "/pages", to: "pages#create"
+  get "/pages/new", to: "pages#new", as: "new_page"
+  get "/pages/:id", to: "pages#show", as: "page"
+  
+ 
+  
   # the get request for pages should go to the pages index method
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
